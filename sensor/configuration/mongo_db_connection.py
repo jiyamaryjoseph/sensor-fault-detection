1f7b34c3d2eb256439f75f2115f0463402106115
jiyamaryjoseph <jiyamaryjoseph@outlook.com>
2022-11-25 12:31:38 +0530

    checked mongodb connection

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,13 +1,15 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+import certifi
+ca=certifi.where()
 class MongoDBClient:
     client = None
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
 
             if MongoDBClient.client is None:
-                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                mongo_db_url="mongodb+srv://jiyamaryjoseph:<jiya7316>@cluster0.5nlu1vw.mongodb.net/?retryWrites=true&w=majority"
+                # mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url="mongodb+srv://jiyamaryjoseph:jiya7316@cluster0.5nlu1vw.mongodb.net/?retryWrites=true&w=majority"
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 

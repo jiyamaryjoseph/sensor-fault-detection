6cc67d8a4675b9d43afe77efa7e96dcb5e30fc39
jiyamaryjoseph <jiyamaryjoseph@outlook.com>
2022-12-02 07:46:51 +0530

    arrange the working tree

--- sensor/constant/training_pipeline/__init__.py
+++ sensor/constant/training_pipeline/__init__.py
@@ -2,24 +2,41 @@ import os
 from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
 
 SAVED_MODEL_DIR =os.path.join("saved_models")
-# defining common constant variable for training pipeline
-TARGET_COLUMN = "class"
+
+
+# defining common constant variable for training pipeline step:1
 PIPELINE_NAME: str = "sensor"
 ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "sensor.csv"
+TARGET_COLUMN = "class"
 
-TRAIN_FILE_NAME: str = "train.csv"
-TEST_FILE_NAME: str = "test.csv"
 
-PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
-MODEL_FILE_NAME = "model.pkl"
-SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
-SCHEMA_DROP_COLS = "drop_columns"
 """
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
+Data Ingestion related constant start with DATA_INGESTION VAR NAME step:1
 """
 DATA_INGESTION_COLLECTION_NAME: str = "sensor"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME step:2
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
+
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+MODEL_FILE_NAME = "model.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+SCHEMA_DROP_COLS = "drop_columns"

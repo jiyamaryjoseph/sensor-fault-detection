9c0639f43efb83150e701bc99308bfb8ceb3bed7
jiyamaryjoseph <jiyamaryjoseph@outlook.com>
2022-12-14 09:11:09 +0530

    put exception

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from sensor.exception import SensorException
 from sensor.configuration import mongo_db_connection
 from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 from sensor.pipeline.training_pipeline import TrainPipeline
+import sys
 
 
 if __name__=='__main__':
+    try:
   
-    training_pipeline=TrainPipeline()
-    training_pipeline.run_pipeline()
+        training_pipeline=TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        raise SensorException(e,sys)
